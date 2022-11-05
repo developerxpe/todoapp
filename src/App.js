22184c2f2b5ac73feeb2bf09f3cbbe4e099744c7
developerxpe <selim223@aof.anadolu.edu.tr>
2022-11-06 01:24:04 +0300

    background img changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Paper from '@material-ui/core/Paper';
 import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
 import UpdateIcon from '@material-ui/icons/Update';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
-
+import background from "./img/bg.png";
 
 function App() {
 
   const [todos, setTodos] = useState(["Todo App With Firebase", "Firebase Curd Operations"])
   const [text, setText] = useState("")
-  
+
 
   // Add
   const Add = (e) => {
@@ -24,12 +24,12 @@ function App() {
   //  Update
   const Update = (i) => {
     const modifyTodo = prompt("Enter New Todo", todos[i])
-    if(modifyTodo){
-    todos[i] = modifyTodo
-    setTodos([...todos])}
-    else
-    {alert("update is cancelled")}
-  
+    if (modifyTodo) {
+      todos[i] = modifyTodo
+      setTodos([...todos])
+    }
+    else { alert("update is cancelled") }
+
   }
   // Delete
   const Delete = (i) => {
@@ -39,11 +39,11 @@ function App() {
   }
   //DeleteAll
   const DeleteAll = () => {
-    if(DeleteAll){
+    if (DeleteAll) {
       alert("are you sure?")
       setTodos([])
     }
-    else{
+    else {
       alert("Delete All op. Cancelled")
     }
   }
@@ -53,14 +53,15 @@ function App() {
 
   return (
 
-    <div>
+    <div style={{ backgroundImage: `url(${background})` }}>
+      <div>
       <h1 className="Todo_Header"> TODO APP</h1>
 
       <form>
         <Paper elevation={5} className="Add_Todo" >
           <TextField className="Text_Field" label="Enter Todos" value={text} onChange={(e) => setText(e.target.value)
           } /><br />
-          <Button type="submit" variant="contained"  disabled={!text} color="primary" onClick={Add}>ADD&nbsp;<AddBoxOutlinedIcon/></Button>
+          <Button type="submit" variant="contained" disabled={!text} color="primary" onClick={Add}>ADD&nbsp;<AddBoxOutlinedIcon /></Button>
           <Button variant="contained" color="primary" onClick={DeleteAll}> Delete All&nbsp;</Button>
 
         </Paper>
@@ -73,8 +74,8 @@ function App() {
 
 
           <div>
-            <Button className="w-24 m-1 items-end" variant="contained" color="primary" onClick={() => Update(i)}> Update&nbsp; <UpdateIcon/>  </Button>&nbsp;&nbsp;
-            <Button className="w-24 m-1 items-end" variant="contained" color="secondary" onClick={() => Delete(i)}> Delete&nbsp; <DeleteOutlineOutlinedIcon/> </Button>&nbsp;&nbsp;
+            <Button className="w-24 m-1 items-end" variant="contained" color="primary" onClick={() => Update(i)}> Update&nbsp; <UpdateIcon />  </Button>&nbsp;&nbsp;
+            <Button className="w-24 m-1 items-end" variant="contained" color="secondary" onClick={() => Delete(i)}> Delete&nbsp; <DeleteOutlineOutlinedIcon /> </Button>&nbsp;&nbsp;
 
 
           </div>
@@ -86,6 +87,7 @@ function App() {
 
       }
 
+    </div>
     </div>
   );
 }
